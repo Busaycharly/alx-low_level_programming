b3e53a2c4f8dc8a3ccb225efd677f647b3a48e1b
Busaycharly <akinsikubusayocharles@gmail.com>
2022-09-13 18:28:12 -0700

    100 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,13 +4,11 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
-
 void print_times_table(int n)
 {
-
 int i, j, k;
 
-f (n >= 0 && n <= 15)
+if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
@@ -47,5 +45,6 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
+}
 }
 }
