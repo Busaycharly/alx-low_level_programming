7e7fc800edfbf6aa7bc03281c22548126b2b13db
Busaycharly <akinsikubusayocharles@gmail.com>
2022-10-12 17:12:47 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,16 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-int num1, num2, op;
+	int num1, num2, op;
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = (*get_op_func(argv[2]))(num1, num2);
+	printf("%d\n", op);
+	return (0);
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-op = (*get_op_func(argv[2]))(num1, num2);
-printf("%d\n", op);
-return (0);
-}
+
