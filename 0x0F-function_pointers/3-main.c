7e8fb8143bb8987200bea93f1a6d27511ac441ea
Busaycharly <akinsikubusayocharles@gmail.com>
2022-10-29 07:25:24 -0700

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,26 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "3-calc.h"
 
 /**
- * main - Prints result of an operation
- * @argc: Number of arguments
- * @argv: Value of arguments
- * Return: Returns an int.
-**/
+ * main - operations between intergers
+ * @argv: argument value array
+ * @argc: argument counter
+ * Return: result of the operation
+ */
 
 int main(int argc, char *argv[])
 {
+int num1, num2, op;
 
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-
-if (
-(strcmp(argv[2], "+") &&
-strcmp(argv[2], "-") &&
-strcmp(argv[2], "*") &&
-strcmp(argv[2], "/") &&
-strcmp(argv[2], "%"))
-)
-{
-printf("Error\n");
-exit(99);
-}
-
-if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
-&& atoi(argv[3]) == 0)
-{
-printf("Error\n");
-exit(100);
-}
-
-printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
-
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op = (*get_op_func(argv[2]))(num1, num2);
+printf("%d\n", op);
 return (0);
 }
